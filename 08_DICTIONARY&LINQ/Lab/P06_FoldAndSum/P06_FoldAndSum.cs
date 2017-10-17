39dde86df3457bc25c1a3ebf05811694e3d9f596
tsekotsolov <tsekotsolov@gmail.com>
2017-10-17 19:47:07 +0300

    foldandsum

--- 08_DICTIONARY&LINQ/Lab/P06_FoldAndSum/P06_FoldAndSum.cs
+++ 08_DICTIONARY&LINQ/Lab/P06_FoldAndSum/P06_FoldAndSum.cs
@@ -10,6 +10,28 @@ namespace P06_FoldAndSum
     {
         static void Main()
         {
+            int[] inputArray = Console.ReadLine()
+                .Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int k = inputArray.Length / 4;
+
+            int[] leftArray = inputArray.Take(k).ToArray();
+            int[] middleArray = inputArray.Skip(k).Take(2 * k).ToArray();
+            int[] rightArray = inputArray.Skip(3 * k).Take(2 * k).ToArray();
+
+
+            int[] mergedArray = leftArray.Reverse().Concat(rightArray.Reverse()).ToArray();
+
+            int[] sumArray = new int[2 * k];
+
+            for (int i = 0; i < 2 * k; i++)
+            {
+                sumArray[i] = mergedArray[i] + middleArray[i];
+            }
+
+            Console.WriteLine(String.Join(" ", sumArray));
 
         }
     }
