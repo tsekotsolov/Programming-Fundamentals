716ea51a1df906a1fa8ef9234d2b7f996a932c73
Tseko Tsolov <tsekotsolov@gmail.com>
2017-10-20 21:00:43 +0300

    upload

---
+++ 09_OBJECT&CLASSES/Exercises/P02_Advertisement Message/P02_ Advertisement Message.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P02_Advertisement_Message
+{
+    class Program
+    {
+        static void Main()
+        {
+            int messageCount = int.Parse(Console.ReadLine());
+
+            string[] phrases = new string[]
+                {"Excellent product.",
+                    "Such a great product.",
+                    "I always use that product.",
+                    "Best product of its category.",
+                    "Exceptional product.",
+                    "I can’t live without this product." };
+
+
+            string[] events = new string[]
+
+                 { "Now I feel good.",
+                "I have succeeded with this product.",
+                "Makes miracles. I am happy of the results!",
+                "I cannot believe but now I feel awesome.",
+                "Try it yourself, I am very satisfied.",
+                "I feel great!" };
+
+            string[] authors = new string[]
+                {"Diana", "Petya", "Stella", "Elena", "Katya", "Iva", "Annie", "Eva"};
+
+            string[] cities = new string[]
+                {"Burgas", "Sofia", "Plovdiv", "Varna", "Ruse"};
+
+            Random rnd = new Random();
+
+            int phraseIndex;
+            int eventsIndex;
+            int authorsIndex;
+            int citiesIndex;
+
+            
+
+            for (int i = 0; i < messageCount; i++)
+            {
+
+                phraseIndex = rnd.Next(phrases.Length);
+                eventsIndex = rnd.Next(events.Length);
+                authorsIndex = rnd.Next(authors.Length);
+                citiesIndex = rnd.Next(cities.Length);
+                Console.WriteLine($"{phrases[phraseIndex]} {events[eventsIndex]} {authors[authorsIndex]} - {cities[citiesIndex]}");
+            }
+
+        }
+    }
+}
---
+++ 09_OBJECT&CLASSES/Exercises/P04_Average Grades/P04_Average Grades.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P04_Average_Grades
+{
+    class Program
+    {
+        class Student
+        {
+            public string Name { get; set; }
+            public double[] Grades { get; set; }
+            public double Average
+            {
+                get
+                {
+                    return Grades.Average();
+                }
+            }
+
+        }
+
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            List<Student> students = new List<Student>();
+
+            while (n-- > 0)
+            {
+                var line = Console.ReadLine().Split();
+                string name = line[0];
+                var grades = line.Skip(1).Select(double.Parse).ToArray();
+
+                Student student = new Student();
+
+
+                student.Name = name;
+                student.Grades = grades;
+                students.Add(student);
+
+            }
+
+
+            students = students
+                .Where(s => s.Average >= 5.00)
+                .OrderBy(s => s.Name)
+                .ThenByDescending(s => s.Average).ToList();
+
+            foreach (var s in students)
+            {
+                Console.WriteLine($"{s.Name} -> {s.Average:f2}");
+            }
+        }
+    }
+}
