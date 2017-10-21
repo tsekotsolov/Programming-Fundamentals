47c722418e92f7a85f0392dd8419969ac79cba9b
Tseko Tsolov <tsekotsolov@gmail.com>
2017-10-21 12:15:49 +0300

    Files and Exceptions

---
+++ 10_FILES&EXEPTIONS/Lab/P01_Odd Lines/P01_Odd Lines.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace P01_Odd_Lines
+{
+    class Program
+    {
+        static void Main()
+        {
+            string contents = File.ReadAllText("input.txt");
+
+            string[] lines = contents.Split(new string[] { Environment.NewLine }, StringSplitOptions.None);
+
+            List<string> output = new List<string>();
+
+
+
+            for (int i = 1; i < lines.Length; i += 2)
+            {
+                output.Add(lines[i]);
+            }
+            File.WriteAllLines("output.txt", output);
+
+
+        }
+    }
+}
