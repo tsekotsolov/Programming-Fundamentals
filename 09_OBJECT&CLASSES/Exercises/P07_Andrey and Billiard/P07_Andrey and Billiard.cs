147c85279cdf9db877dd55d236c32ac1289d5e1c
tsekotsolov <tsekotsolov@gmail.com>
2017-10-25 18:10:39 +0300

    push

--- 09_OBJECT&CLASSES/Exercises/P07_Andrey and Billiard/P07_Andrey and Billiard.cs
+++ 09_OBJECT&CLASSES/Exercises/P07_Andrey and Billiard/P07_Andrey and Billiard.cs
@@ -11,11 +11,8 @@ namespace P07_Andrey_and_Billiard
         class Customer
         {
             public string Name { get; set; }
-
-            public Dictionary<string, int> ProductQuantityDict = new Dictionary<string, int>();
-            public decimal TotalPrice { get; set; }
-            //public string DesiredProduct { get; set; }
-            //public int Quantity { get; set; }
+            public Dictionary<string, int> CustomerWishList { get; set; } = new Dictionary<string, int>();
+            public decimal ClientBill { get; set; }
 
         }
         static void Main()
@@ -30,32 +27,104 @@ namespace P07_Andrey_and_Billiard
 
                 if (!menu.ContainsKey(inputLine[0]))
                 {
-                    menu[inputLine[0]] = decimal.Parse(inputLine[1]);
+                    menu[inputLine[0]] = 0;
                 }
 
                 menu[inputLine[0]] = decimal.Parse(inputLine[1]);
             }
 
-           
 
-            while (true)
+
+
+            var line = Console.ReadLine();
+
+            var nameWishList = new Dictionary<string, Dictionary<string, int>>();
+            var nameBill = new SortedDictionary<string, decimal>();
+            var shopList = new Dictionary<string, int>();
+            var totalBill = 0.0M;
+
+            while (line != "end of clients")
             {
-                var customersInput = Console.ReadLine().Split('-',',');
+
+                var customersInput = line.Split('-', ',');
+
 
                 Customer currentCustomer = new Customer();
 
-                currentCustomer = createCustomer(customersInput);
+                //currentCustomer = createCustomer(customersInput);
+
+                currentCustomer.Name = customersInput[0];
+                currentCustomer.CustomerWishList[customersInput[1]] = int.Parse(customersInput[2]); 
+
+                if (!nameWishList.ContainsKey(currentCustomer.Name) && currentCustomer.CustomerWishList.ContainsKey(customersInput[1]) == menu.ContainsKey(customersInput[1]))
+                {
+                    nameWishList[currentCustomer.Name] = currentCustomer.CustomerWishList;
+
+                }
+                if (nameWishList.ContainsKey(currentCustomer.Name))
+                {
+                    foreach (var name in nameWishList)
+                    {
+                        foreach (var productqtyPair in name.Value)
+                        {
+                            if (productqtyPair.Key.Contains(customersInput[1]))
+                            {
+                                productqtyPair.Value += int.Parse(customersInput[2]);
+                            }
+                        }
+                    }
+                }
+
+                if (!nameBill.ContainsKey(currentCustomer.Name) && nameWishList.ContainsKey(currentCustomer.Name))
+                {
+
+                    currentCustomer.ClientBill = int.Parse(customersInput[2]) * menu[customersInput[1]];
+
+                    nameBill[currentCustomer.Name] = currentCustomer.ClientBill;
+
+                    shopList[customersInput[1]] = int.Parse(customersInput[2]);
+                }
+
+
+                totalBill += currentCustomer.ClientBill;
+
+                line = Console.ReadLine();
+            }
+
+
+
+
+            foreach (var client in nameBill)
+            {
+                Console.WriteLine(client.Key);
+
+                foreach (var item in nameWishList)
+                {
+                    if (client.Key == item.Key)
+                    {
+                        Console.Write($"-- {string.Join(" ", item.Value.Keys)} -");
+                        Console.WriteLine($" {string.Join(" ", item.Value.Values)}");
+                    }
+
+
+
+                }
+
+                Console.WriteLine($"Bill: {client.Value:f2}");
 
             }
+
+
+            Console.WriteLine($"Total bill: {totalBill:f2}");
+
         }
 
-        private static Customer createCustomer(string[] customers)
-        {
-            Customer customer = new Customer();
-
-            customer.Name = customers[0];
-            customer.ProductQuantityDict[customers[1]] = int.Parse(customers[2]);
-            return customer; 
-        }
+        //private static Customer createCustomer(string[] customers)
+        //{
+        //    Customer customer = new Customer();
+        //    customer.Name = customers[0];
+        //    customer.CustomerWishList[customers[1]] = int.Parse(customers[2]);
+        //    return customer;
+        //}
     }
 }
---
+++ 09_OBJECT&CLASSES/Exercises/P08_MentorGroup/P08_MentorGroup.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Globalization;
+
+namespace P08_MentorGroup
+{
+    class P08_MentorGroup
+    {
+        class Student
+        {
+            public List<string> Comments { get; set; } = new List<string>();
+            public List<DateTime> Dates { get; set; } = new List<DateTime>();
+            public string Name { get; set; }
+        }
+
+        static void Main()
+        {
+            var inputLineOfDates = Console.ReadLine();
+
+            SortedDictionary<string, Student> studentsDict = new SortedDictionary<string, Student>();
+
+            while (inputLineOfDates != "end of dates")
+            {
+
+                var input = inputLineOfDates.Split(' ', ',');
+
+                var name = input.First();
+
+                var dates = input
+                    .Skip(1)
+                    .Select(x => DateTime.ParseExact(x, "dd/MM/yyyy", CultureInfo.InvariantCulture))
+                    .Distinct()
+                    .OrderBy(x => x)
+                    .ToList();
+
+                Student currentstudent = new Student()
+                {
+                    Name = name,
+                    Dates = dates
+                };
+                if (!studentsDict.ContainsKey(name))
+
+                {
+                    studentsDict.Add(name, currentstudent);
+                }
+
+                else
+                {
+                    studentsDict[name].Dates.AddRange(currentstudent.Dates);
+                }
+
+                inputLineOfDates = Console.ReadLine();
+            }
+
+            var inputLineOfComments = Console.ReadLine();
+
+            while (inputLineOfComments != "end of comments")
+            {
+                var input = inputLineOfComments.Split('-');
+
+                var name = input.First();
+
+                var comments = input
+                    .Skip(1)
+                    .OrderBy(x => x)
+                    .ToList();
+
+                if (studentsDict.ContainsKey(name))
+
+                {
+                    Student currentstudent = new Student()
+                    {
+                        Name = name,
+                        Comments = comments
+                    };
+                    studentsDict[name].Comments.AddRange(currentstudent.Comments);
+                }
+
+                inputLineOfComments = Console.ReadLine();
+            }
+
+            foreach (var student in studentsDict)
+            {
+                Console.WriteLine(student.Key);
+
+                Console.WriteLine("Comments:");
+                foreach (var item in student.Value.Comments)
+                {
+
+                    Console.WriteLine($"- {item}");
+                }
+
+                Console.WriteLine("Dates attended:");
+                foreach (var item in student.Value.Dates)
+                {
+
+                    Console.WriteLine($"-- {item:dd/MM/yyyy}");
+                }
+
+            }
+
+        }
+
+    }
+}
