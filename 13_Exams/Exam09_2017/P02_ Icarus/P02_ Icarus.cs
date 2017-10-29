bb84ef7b3f2be8812ec1c6810cd808b3f340f184
tsekotsolov <tsekotsolov@gmail.com>
2017-10-29 17:58:32 +0200

    P02_ Icarus.cs

--- 13_Exams/Exam09_2017/P02_ Icarus/P02_ Icarus.cs
+++ 13_Exams/Exam09_2017/P02_ Icarus/P02_ Icarus.cs
@@ -11,35 +11,64 @@ namespace P02__Icarus
         static void Main()
         {
             var inputList = Console.ReadLine().Split().Select(int.Parse).ToList();
-            int initialPosition = int.Parse(Console.ReadLine());
+            int indexPosition = int.Parse(Console.ReadLine());
             int damage = 1;
+            
+
 
             var moves = Console.ReadLine();
             while (moves != "Supernova")
             {
                 var movesSplited = moves.Split(' ').ToArray();
-
                 var direction = movesSplited.First();
                 var steps = int.Parse(movesSplited[1]);
 
-                if (direction == "left")
+                if (direction == "left" && steps != 0)
                 {
-                    for (int i = initialPosition; i > initialPosition - steps; i--)
-                    {
-                        inputList[i - 1] = inputList[i - 1] - damage;
-                        
-                    }
-                    initialPosition = initialPosition-steps;
 
+                    for (int i = 1; i <= steps; i++)
+                    {
+                        if (indexPosition <= 0)
+                        {
+                            damage++;
+                            indexPosition = inputList.Count;
+                            
+                        }
+
+                        inputList[indexPosition - 1] = inputList[indexPosition - 1] - damage;
+
+                        indexPosition--;
+
+                    }
+
+                }
+
+                else if (direction == "right" && steps != 0)
+                {
+
+                    for (int i = 1; i <= steps; i++)
+                    {
+
+                        if (indexPosition >= inputList.Count-1)
+                        {
+                            damage++;
+                            indexPosition = -1;
+                           
+                        }
+
+                        inputList[indexPosition + 1] = inputList[indexPosition + 1] - damage;
+                        indexPosition++;
+
+                    }
                 }
 
                 else
                 {
-                    
-
+                    moves = Console.ReadLine();
+                    continue;
                 }
 
-              
+
                 moves = Console.ReadLine();
             }
 
