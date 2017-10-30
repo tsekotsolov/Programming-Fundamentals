ccf18e3f4e54c324af9fa4ec3d998f48d90af413
tsekotsolov <tsekotsolov@gmail.com>
2017-10-30 19:34:42 +0200

    P01_Resurrection

---
+++ 13_Exams/Exam09_2017/P01_Resurrection/P01_Resurrection.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+
+
+namespace P01_Resurrection
+{
+    class P01_Resurrection
+    {
+        static void Main()
+        {
+            int numberOfBirds = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < numberOfBirds; i++)
+            {
+                long lenght = long.Parse(Console.ReadLine());
+                decimal width = decimal.Parse(Console.ReadLine());
+                long wing = long.Parse(Console.ReadLine());
+
+                var delta = width - Math.Floor(width);
+                var precision = 0;
+
+                if (delta!=0)
+                {
+                    precision = delta.ToString().Split('.').Skip(1).ToArray().First().ToString().Length;
+                }
+                
+                decimal years = lenght * lenght * (width + 2 * wing);
+
+                Console.WriteLine($"{Math.Round(years,precision)}");
+
+            }
+        }
+    }
+}
