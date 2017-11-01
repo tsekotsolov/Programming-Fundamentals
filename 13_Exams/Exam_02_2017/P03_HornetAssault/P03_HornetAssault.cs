f5b1e9f49811dada1b9fe8ed356441533342a88d
tsekotsolov <tsekotsolov@gmail.com>
2017-11-01 13:21:44 +0200

    hornet exam almost solved

--- 13_Exams/Exam_02_2017/P03_HornetAssault/P03_HornetAssault.cs
+++ 13_Exams/Exam_02_2017/P03_HornetAssault/P03_HornetAssault.cs
@@ -1,23 +1,49 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace P03_HornetAssault
 {
     class P03_HornetAssault
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            string s = "ivan";
+            var beeHives = Console.ReadLine().Split().Select(long.Parse).ToList();
+            var hornets = Console.ReadLine().Split().Select(long.Parse).ToList();
 
-            
 
+            var hornetsPower = hornets.Sum();
 
-            char[] arr = s.ToCharArray();
-            Array.Reverse(arr);
-            var reversed =  new string(arr);
+            for (int i = 0; i < beeHives.Count; i++)
+            {
+                if (hornetsPower > beeHives[i])
+                {
+                    beeHives[i] = 0;
+                }
+                else
+                {
+                    beeHives[i] -= hornetsPower;
+
+                    if (hornets.Count>0)
+                    {
+                        hornets.RemoveAt(0);
+                    }
+
+                    hornetsPower = hornets.Sum();
+                }
+            }
+
+            if (beeHives.Any(x => x != 0))
+            {
+                beeHives.RemoveAll(x => x == 0);
+                Console.WriteLine(string.Join(" ", beeHives));
+            }
+
+            else if (hornets.Count != 0)
+            {
+                Console.WriteLine(string.Join(" ", hornets));
+            }
+
         }
     }
 }
