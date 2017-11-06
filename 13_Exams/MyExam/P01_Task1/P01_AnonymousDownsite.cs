4052b3b93ced5a198932a2bd156917ce91462e4f
Tseko Tsolov <tsekotsolov@gmail.com>
2017-11-06 08:18:42 +0200

    exam

---
+++ 13_Exams/MyExam/P01_Task1/P01_AnonymousDownsite.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+
+
+namespace AnonymousDownsite
+{
+    
+    class P01_AnonymousDownsite
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            int securityKey = int.Parse(Console.ReadLine());
+
+            decimal totalLoss = 0M;
+
+            List<string> affectedSites = new List<string>();
+
+            for (int i = 0; i < n; i++)
+            {
+                var input = Console.ReadLine().Split().ToList();
+
+                var siteName = input[0];
+                long siteVisits = long.Parse(input[1]);
+                decimal sitePricePerVisit = decimal.Parse(input[2]);
+
+                totalLoss += siteVisits * sitePricePerVisit;
+
+                affectedSites.Add(siteName);
+            }
+
+            for (int i = 0; i < affectedSites.Count; i++)
+            {
+                Console.WriteLine(affectedSites[i]);
+            }
+
+            Console.WriteLine($"Total Loss: {totalLoss:F20}");
+
+            Console.WriteLine($"Security Token: {BigInteger.Pow(new BigInteger(securityKey), n)}");
+
+        }
+    }
+}
---
+++ 13_Exams/MyExam/P04_Task4/Program.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P04_Task4
+{
+    class Program
+    {
+
+        class Data
+        {
+            public string DataSet { get; set; }
+            public int DataSize { get; set; }
+            public string DataKey { get; set; }
+        }
+        static void Main()
+        {
+            var inputLine = Console.ReadLine();
+
+
+            List<string> dataSetList = new List<string>();
+
+            Dictionary<string, List<Data>> MajorData = new Dictionary<string, List<Data>>();
+
+            Dictionary<string, List<Data>> Cache = new Dictionary<string, List<Data>>();
+
+            while (inputLine != "thetinggoesskrra")
+            {
+                var input = inputLine.Split(new char[] { ' ', '|', '-', '>' }, StringSplitOptions.RemoveEmptyEntries).ToList();
+
+
+                List<Data> listOfData = new List<Data>();
+
+                if (input.Count == 1)
+                {
+                    var dataSet = input[0];
+
+                    if (!MajorData.ContainsKey(dataSet))
+                    {
+                        dataSetList.Add(dataSet);
+                    }
+
+                    if (Cache.ContainsKey(dataSet))
+                    {
+                        MajorData.Add(dataSet, Cache[dataSet]);
+                    }
+                }
+
+                else
+                {
+                    var dataKey = input[0];
+                    var dataSize = int.Parse(input[1]);
+                    var dataSet = input[2];
+
+                    Data data = new Data()
+                    {
+                        DataSet = dataSet,
+                        DataKey = dataKey,
+                        DataSize = dataSize,
+                    };
+
+                    listOfData.Add(data);
+
+                    if (dataSetList.Contains(dataSet) && !MajorData.ContainsKey(dataSet))
+
+                    {
+                        MajorData.Add(dataSet, listOfData);
+                    }
+
+                    else if (dataSetList.Contains(dataSet) && MajorData.ContainsKey(dataSet))
+                    {
+                        MajorData[dataSet].AddRange(listOfData);
+                    }
+
+                    if (!MajorData.ContainsKey(dataSet))
+                    {
+                        if (!Cache.ContainsKey(dataSet))
+                        {
+                            Cache.Add(dataSet, listOfData);
+                        }
+
+
+                        else if (Cache.ContainsKey(dataSet))
+                        {
+                            Cache[dataSet].AddRange(listOfData);
+                        }
+                    }
+
+
+                }
+
+                inputLine = Console.ReadLine();
+            }
+
+
+            foreach (var data in MajorData)
+            {
+                foreach (var item in MajorData.Values)
+                {
+                   
+                }
+
+            }
+        }
+    }
+}
