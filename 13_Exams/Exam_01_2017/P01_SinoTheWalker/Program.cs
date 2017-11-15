5e6f301eb22c654475f7e8c2e95997f9841eb18f
tsekotsolov <tsekotsolov@gmail.com>
2017-11-15 14:43:45 +0200

    Exam added

---
+++ 13_Exams/Exam_01_2017/P01_SinoTheWalker/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace P01_SinoTheWalker
+{
+    class Program
+    {
+        static void Main()
+        {
+            DateTime timeOfLeavingSoftUni = DateTime.Parse(Console.ReadLine());
+            //int step = int.Parse(Console.ReadLine()) % 86400;
+            //int seconds = int.Parse(Console.ReadLine()) % 86400;
+            //86400 секунди = 1 ден
+            //махаме цели дни, ако някой се прави на интересен да ни ги подава като вход
+
+            int stepsTaken = int.Parse(Console.ReadLine()) % 86400;
+            int stepsPerSec = int.Parse(Console.ReadLine()) % 86400;
+            int timeInTravel = stepsTaken * stepsPerSec;
+            DateTime result = timeOfLeavingSoftUni.AddSeconds(timeInTravel);
+          
+            Console.WriteLine("Time Arrival: {0:HH:mm:ss}", result);
+
+        }
+    }
+}
