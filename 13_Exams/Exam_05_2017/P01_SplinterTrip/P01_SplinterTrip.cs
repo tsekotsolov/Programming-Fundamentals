d3a38a9180a5816ac4f543e932a41ee3094003fb
Tseko Tsolov <tsekotsolov@gmail.com>
2017-11-17 16:51:59 +0200

    Exam added

---
+++ 13_Exams/Exam_05_2017/P01_SplinterTrip/P01_SplinterTrip.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+
+namespace P01_SplinterTrip
+{
+    class P01_SplinterTrip
+    {
+        static void Main()
+        {
+            decimal distance = decimal.Parse(Console.ReadLine());
+            decimal fuelLoaded = decimal.Parse(Console.ReadLine());
+            decimal milesInWinds = decimal.Parse(Console.ReadLine());
+
+            var fuelNeeded = ((milesInWinds * 25 * 1.5m) 
+                + (distance - milesInWinds) * 25) * 1.05m;
+
+            Console.WriteLine($"Fuel needed: {fuelNeeded:f2}L");
+
+            if (fuelLoaded>=fuelNeeded)
+            {
+                Console.WriteLine($"Enough with {(fuelLoaded-fuelNeeded):f2}L to spare!");
+            }
+
+            else
+            {
+                Console.WriteLine($"We need {(fuelNeeded - fuelLoaded):f2}L more fuel.");
+            }
+
+        }
+    }
+}
