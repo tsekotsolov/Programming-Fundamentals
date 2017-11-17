4d5ec741728e4da558b74260e7e61afeac9aa48b
tsekotsolov <tsekotsolov@gmail.com>
2017-11-17 11:54:20 +0200

    changes

---
+++ 13_Exams/WormExam/P03_WormHole/P03_WormHole.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P03_WormHole
+{
+    class P03_WormHole
+    {
+        static void Main()
+        {
+            var inputLine = Console.ReadLine().Split().Select(int.Parse).ToList();
+            var steps = 0;
+
+            for (int i = 0; i < inputLine.Count; i++)
+            {
+                if (inputLine[i]==0)
+                {
+                    steps++;
+                }
+
+                else
+                {
+                    var currentValueAti = inputLine[i];
+                    inputLine[i] = 0;
+                    i = currentValueAti-1;
+                    
+                }
+               
+            }
+
+            Console.WriteLine(steps);
+        }
+    }
+}
