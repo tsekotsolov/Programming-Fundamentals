57cbcb563ef0d4ecae31a0a53f83c32cf7ddcdf4
tsekotsolov <tsekotsolov@gmail.com>
2017-11-18 10:09:54 +0200

    exam added

---
+++ 13_Exams/Exam_05_2017/P03_Spyfer/P03_Spyfer.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+
+
+namespace P03_Spyfer
+{
+    class P03_Spyfer
+    {
+        static void Main()
+        {
+            var inputLine = Console.ReadLine().Split().Select(int.Parse).ToList();
+
+            for (int i = 0; i < inputLine.Count; i++)
+            {
+
+                if (i != 0 && i != inputLine.Count - 1)
+                {
+                    if (inputLine[i] == inputLine[i - 1] + inputLine[i + 1])
+                    {
+                        inputLine.RemoveAt(i + 1);
+                        inputLine.RemoveAt(i - 1);
+                        i = 0;
+                    }
+                }
+
+                else if (i == 0 && inputLine[i] == inputLine[i + 1])
+                {
+                    inputLine.RemoveAt(i + 1);
+                    i = 0;
+                }
+
+                else if (i == inputLine.Count - 1 && inputLine[i] == inputLine[i - 1])
+                {
+                    inputLine.RemoveAt(i - 1);
+                    i = 0;
+                }
+
+            }
+            Console.WriteLine(string.Join(" ", inputLine));
+
+        }
+    }
+}
---
+++ 13_Exams/Exam_05_2017/P04_NSA/P04_NSA.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P04_NSA
+{
+    class P04_NSA
+    {
+        class Spy
+        {
+            public string SpyName { get; set; }
+            public long DaysInService { get; set; }
+        }
+        static void Main()
+        {
+            var inputLine = Console.ReadLine();
+
+            var spyRegister = new Dictionary<string, List<Spy>>();
+
+
+            while (inputLine != "quit")
+            {
+
+                var input = inputLine.Split(new[] { " -> " }, StringSplitOptions.None).ToList();
+
+                var countryName = input[0];
+                var spyName = input[1];
+                var daysInService = long.Parse(input[2]);
+
+
+                Spy spy = new Spy()
+                {
+                    SpyName = spyName,
+                    DaysInService = daysInService
+
+                };
+
+                if (!spyRegister.ContainsKey(countryName))
+                {
+                    spyRegister[countryName] = new List<Spy>();
+                }
+
+                else if (spyRegister[countryName].Any(x => x.SpyName.Contains(spyName)))
+                {
+                    spyRegister[countryName].RemoveRange(0, 1);
+                }
+
+
+                spyRegister[countryName].Add(spy);
+
+                inputLine = Console.ReadLine();
+
+            }
+
+            var sortedSpyRegister = spyRegister.OrderByDescending(x => x.Value.Count);
+
+            foreach (var country in sortedSpyRegister)
+            {
+                Console.WriteLine($"Country: {country.Key}");
+
+                foreach (var spy in country.Value.OrderByDescending(x => x.DaysInService))
+                {
+                    Console.WriteLine($"**{spy.SpyName} : {spy.DaysInService}");
+                }
+            }
+        }
+    }
+}
