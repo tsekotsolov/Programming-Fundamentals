54cd2184838b54c0b8d982ab1a1e3c8a6a63bc40
tsekotsolov <tsekotsolov@gmail.com>
2017-11-22 15:39:50 +0200

    up

---
+++ 11_STRINGS/Exercises/P05_MagicExchangeableWords/P05_MagicExchangeableWords.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P05_MagicExchangeableWords
+{
+    class P05_MagicExchangeableWords
+    {
+        static void Main()
+        {
+            var input = Console.ReadLine().Split().ToArray();
+
+            var firstWord = input[0];
+            var secondWord = input[1];
+
+            var cycleItterantions = Math.Min(firstWord.Length, secondWord.Length);
+            var isMagic = "true";
+            var checkDict = new Dictionary<char, char>();
+
+            if (firstWord.Length < secondWord.Length)
+            {
+                var temp = secondWord;
+                secondWord = firstWord;
+                firstWord = temp;
+            }
+
+            for (int i = 0; i < cycleItterantions; i++)
+            {
+                if (!checkDict.ContainsKey(firstWord[i]))
+                {
+                    checkDict.Add(firstWord[i], secondWord[i]);
+                }
+
+                else
+                {
+                    if (checkDict[firstWord[i]] != secondWord[i])
+                    {
+                        isMagic = "false";
+                        Console.WriteLine(isMagic);
+                        return;
+                    }
+                }
+            }
+
+            if (firstWord.Length != secondWord.Length)
+            {
+                var remainingWord = firstWord.Substring(secondWord.Length);
+
+                for (int i = 0; i < remainingWord.Length; i++)
+                {
+                    if (!checkDict.ContainsKey(remainingWord[i]))
+                    {
+                        isMagic = "false";
+                    }
+                }
+            }
+
+            Console.WriteLine(isMagic);
+        }
+    }
+}
---
+++ 11_STRINGS/Exercises/P08_Letters Change Numbers/P08_Letters Change Numbers.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P08_Letters_Change_Numbers
+{
+    class Program
+    {
+        static void Main()
+        {
+            var input = Console.ReadLine();
+
+            bool isUpper;
+            var letterPosition = 0;
+
+            if (input[0] >= 65 && input[0] <= 90) 
+            {
+                isUpper = true;
+                letterPosition = input[0] - 65;
+            }
+
+
+
+
+
+        }
+    }
+}
