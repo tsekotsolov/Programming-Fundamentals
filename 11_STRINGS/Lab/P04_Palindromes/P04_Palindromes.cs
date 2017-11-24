732a245a70807cd6f2844f16616795fca7753b22
Tseko Tsolov <tsekotsolov@gmail.com>
2017-11-24 14:46:59 +0200

    up

--- 11_STRINGS/Lab/P04_Palindromes/P04_Palindromes.cs
+++ 11_STRINGS/Lab/P04_Palindromes/P04_Palindromes.cs
@@ -1,8 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+
 
 namespace P04_Palindromes
 {
@@ -10,25 +9,45 @@ namespace P04_Palindromes
     {
         static void Main()
         {
+            var input = Console.ReadLine().Split(new char[] { ' ', '.', '?', '!', ',' }, StringSplitOptions.RemoveEmptyEntries);
 
-            var input = Console.ReadLine();
+            var palindromeDict = new SortedDictionary<string, int>();
 
-            var namesDict = new Dictionary<string, string>();
-            Random rand = new Random();
-
-            while (input!="end")
+            for (int i = 0; i < input.Length; i++)
             {
-                namesDict.Add(input, input);
-                input = Console.ReadLine();
+                var isPalindrome = ChecksPalindrome(input[i]);
+
+                if (isPalindrome)
+                {
+                    if (!palindromeDict.ContainsKey(input[i]))
+                    {
+                        palindromeDict.Add(input[i], i);
+                    }
+                }
             }
 
-            namesDict = namesDict.OrderBy(x => rand.Next())
-              .ToDictionary(item => item.Key, item => item.Value);
+            Console.WriteLine(string.Join(", ", palindromeDict.Keys));
+        }
 
-            foreach (var name  in namesDict)
+        private static bool ChecksPalindrome(string input)
+        {
+            var isPalindrome = false;
+            var reversedBag = new StringBuilder();
+
+            for (int i = 0; i < input.Length; i++)
             {
-                Console.WriteLine($"{name.Value} - {name.Key}");
+                reversedBag.Append(input[input.Length - i - 1]);
             }
+
+            var reversedString = reversedBag.ToString();
+
+
+            if (reversedString == input)
+            {
+                isPalindrome = true;
+            }
+
+            return isPalindrome;
         }
     }
 }
---
+++ 11_STRINGS/Lab/P05_Randomizer/P05_Randomizer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P05_Randomizer
+{
+    class P05_Randomizer
+    {
+        static void Main()
+        {
+            var input = Console.ReadLine();
+
+            var namesDict = new Dictionary<string, string>();
+            Random rand = new Random();
+
+            while (input != "end")
+            {
+                namesDict.Add(input, input);
+                input = Console.ReadLine();
+            }
+
+            namesDict = namesDict.OrderBy(x => rand.Next())
+              .ToDictionary(item => item.Key, item => item.Value);
+
+            foreach (var name in namesDict)
+            {
+                Console.WriteLine($"{name.Value} - {name.Key}");
+            }
+        }
+    }
+}
