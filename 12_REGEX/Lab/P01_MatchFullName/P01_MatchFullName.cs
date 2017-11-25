173c2a39628376e3b2a512e1ad11a8140773c938
tsekotsolov <tsekotsolov@gmail.com>
2017-11-25 12:23:26 +0200

    Lab

---
+++ 12_REGEX/Lab/P01_MatchFullName/P01_MatchFullName.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace P01_MatchFullName
+{
+    class P01_MatchFullName
+    {
+        static void Main()
+        {
+            string pattern = @"\b[A-Z][a-z]+ [A-Z][a-z]+";
+
+            var text = Console.ReadLine();
+
+            MatchCollection names = Regex.Matches(text, pattern);
+
+            foreach (Match name in names)
+            {
+                Console.Write(name.Value + " ");
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
