9bb432826e798975ffe9c0c5b850e4cfdb36cea1
tsekotsolov <tsekotsolov@gmail.com>
2017-11-27 16:39:30 +0200

    up

--- 13_Exams/TrainExam/P03_Trainegram/P03_Trainegram.cs
+++ 13_Exams/TrainExam/P03_Trainegram/P03_Trainegram.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 
 namespace P03_Trainegram
 {
@@ -10,6 +11,26 @@ namespace P03_Trainegram
     {
         static void Main()
         {
+            
+            var pattern = @"^(<\[([^A-Za-z0-9]+?)?\]\.)((\.\[([A-Za-z0-9+]+)?\]\.)+)?$";
+
+            var input =Console.ReadLine();
+
+            var bag = new StringBuilder();
+
+            while (input!= "Traincode!")
+            {
+                var trainisFound = Regex.Match(input, pattern).Success;
+
+                if (trainisFound)
+                {
+                    bag.AppendLine(input);
+                }
+                input =Console.ReadLine();
+            }
+
+            var trains = bag.ToString().TrimEnd();
+            Console.WriteLine(trains);
         }
     }
 }
