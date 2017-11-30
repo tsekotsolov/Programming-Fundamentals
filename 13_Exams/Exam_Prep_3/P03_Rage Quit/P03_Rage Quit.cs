f7880a71e7cb6dd4aed3467986cedfd7185c08bf
tsekotsolov <tsekotsolov@gmail.com>
2017-11-30 19:18:45 +0200

    up

--- 13_Exams/Exam_Prep_3/P03_Rage Quit/P03_Rage Quit.cs
+++ 13_Exams/Exam_Prep_3/P03_Rage Quit/P03_Rage Quit.cs
@@ -1,8 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 
 namespace P03_Rage_Quit
 {
@@ -10,6 +9,45 @@ namespace P03_Rage_Quit
     {
         static void Main(string[] args)
         {
+            var input = Console.ReadLine().ToUpper();
+            var fullpatern = @"(\D+)(\d+)";
+
+
+            var match = Regex.Matches(input, fullpatern);
+            var digitList = new List<int>();
+            var symbolList = new List<string>();
+
+            foreach (Match item in match)
+            {
+                symbolList.Add(item.Groups[1].ToString());
+
+                digitList.Add(int.Parse(item.Groups[2].ToString()));
+            }
+
+
+            var bag = new StringBuilder();
+
+            for (int i = 0; i < digitList.Count; i++)
+            {
+                for (int j = 0; j < digitList[i]; j++)
+                {
+                    bag.Append(symbolList[i]);
+                }
+            }
+
+            var uniqueList = new List<char>();
+
+            foreach (var ch in bag.ToString())
+            {
+                if (!uniqueList.Contains(ch))
+                {
+                    uniqueList.Add(ch);
+                }
+            }
+
+            Console.WriteLine($"Unique symbols used: {uniqueList.Count}");
+            Console.WriteLine(bag);
+
         }
     }
 }
