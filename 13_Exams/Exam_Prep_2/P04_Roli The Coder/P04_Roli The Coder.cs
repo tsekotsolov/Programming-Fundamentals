ea301c74494e33eab1e4c8053396680b1da0ff58
tsekotsolov <tsekotsolov@gmail.com>
2017-12-05 19:38:28 +0200

    up

--- 13_Exams/Exam_Prep_2/P04_Roli The Coder/P04_Roli The Coder.cs
+++ 13_Exams/Exam_Prep_2/P04_Roli The Coder/P04_Roli The Coder.cs
@@ -1,15 +1,90 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
+
 
 namespace P04_Roli_The_Coder
 {
     class Program
     {
+        class Event
+        {
+            public string Name { get; set; }
+            public List<string> Participants { get; set; }
+
+        }
         static void Main()
         {
+
+            var input = Console.ReadLine();
+
+            var eventList = new Dictionary<string, Event>();
+
+            while (input != "Time for Code")
+            {
+                var tokens = input.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+                string eventName;
+
+                var eventId = tokens[0];
+
+                if (tokens[1].StartsWith("#"))
+                {
+                    eventName = tokens[1].Substring(1);
+                }
+                else
+                {
+                    input = Console.ReadLine();
+                    continue;
+                }
+
+                var participantList = new List<string>();
+
+                for (int i = 2; i < tokens.Length; i++)
+                {
+                    if (!participantList.Contains(tokens[i]))
+                    {
+
+                        participantList.Add(tokens[i]);
+
+                    }
+                }
+
+                Event currentEvent = new Event();
+                currentEvent.Name = eventName;
+                currentEvent.Participants = participantList;
+
+
+                if (!eventList.ContainsKey(eventId))
+                {
+                    eventList.Add(eventId, currentEvent);
+                }
+
+                else if (eventList[eventId].Name == eventName)
+                {
+                    foreach (var person in currentEvent.Participants)
+                    {
+                        if (!eventList[eventId].Participants.Contains(person))
+                        {
+                            eventList[eventId].Participants.Add(person);
+                        }
+                    }
+                }
+
+                input = Console.ReadLine();
+            }
+
+            foreach (var id in eventList.OrderByDescending(x => x.Value.Participants.Count).ThenBy(x => x.Value.Name))
+            {
+                Console.WriteLine($"{id.Value.Name} - {id.Value.Participants.Count}");
+
+                foreach (var person in id.Value.Participants.OrderBy(x => x))
+                {
+                    Console.WriteLine($"{person}");
+                }
+            }
+
         }
     }
 }
