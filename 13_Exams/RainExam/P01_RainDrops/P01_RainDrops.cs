dd406fe109c35d86a708e6600cbdf83f1152277c
Tseko Tsolov <tsekotsolov@gmail.com>
2017-12-10 17:06:09 +0200

    up

--- 13_Exams/RainExam/P01_RainDrops/P01_RainDrops.cs
+++ 13_Exams/RainExam/P01_RainDrops/P01_RainDrops.cs
@@ -1,9 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Numerics;
+
 
 namespace P01_RainDrops
 {
@@ -11,6 +8,40 @@ namespace P01_RainDrops
     {
         static void Main()
         {
+            var n = int.Parse(Console.ReadLine());
+
+            var density = double.Parse(Console.ReadLine());
+
+            double sumOfRegionalCoeficients = 0.0;
+
+            for (int i = 0; i < n; i++)
+            {
+                var inputLine = Console.ReadLine().Split().ToArray();
+
+                var rainDropsCount = double.Parse(inputLine[0]);
+
+                var squareMeters = double.Parse(inputLine[1]);
+
+                var regionalCoef = rainDropsCount / squareMeters;
+
+                sumOfRegionalCoeficients += regionalCoef;
+            }
+
+            if (density!=0)
+            {
+                var result = sumOfRegionalCoeficients / density;
+
+
+                Console.WriteLine($"{result:f3}");
+            }
+
+            else
+            {
+                Console.WriteLine($"{sumOfRegionalCoeficients:f3}");
+            }
+
+            
+
         }
     }
 }
